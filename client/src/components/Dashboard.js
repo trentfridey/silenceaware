08e94fd01f7e6113689e52637d519cdf732b5f42
Trent Fridey <trentfridey@colorado.edu>
2018-02-04 18:34:48 -0600

    Fix onSubmit function

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,23 @@ import React from 'react';
 import '../index.css';
 import DateTime from 'react-datetime';
 import './DateTime.css';
+import {Validate, ValidateGroup, ErrorMessage} from 'react-validate';
+
+// Validation setup
+
+// const validateAfter = (after) => {
+//     validateRange(after, this.)
+// }
+
+// const validateBefore = (before) => {
+
+// }
+
+// const validateRange = (after, before) => {
+
+// }
+
+// Main Component
 
 export default class Dashboard extends React.Component {
     constructor(props){
@@ -9,11 +26,12 @@ export default class Dashboard extends React.Component {
         this.state = {};
         this.state.events = []
         this.getEvents = this.getEvents.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     getEvents(after, before){
-        before = before.toISOString();
-        after = after.toISOString();
+        before = (new Date(before)).toISOString();
+        after = (new Date(after)).toISOString();
         var url = "https://event-server.glitch.me"
         //const url = "https://ax65c8djp8.execute-api.us-west-2.amazonaws.com"
         return fetch(`${url}/events/${after}/${before}`,
@@ -23,6 +41,26 @@ export default class Dashboard extends React.Component {
                 headers: {'Content-Type':'application/json'},
             }).then(res => res.json())
             .catch(err => {throw err})
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        const data = new FormData(e.target)
+        let before = data.get("before");
+        let after = data.get("after");
+        if(after === null || before === null){
+            after = (new Date(Date.now()-1000*60*60));
+            before = (new Date());
+        }
+        this.getEvents(after, before).then(data =>{
+            let activeEvents = data.filter((ev)=> ev.end_time === "null")
+            let pastEvents = data.filter((ev)=>ev.end_time !== "null")
+            pastEvents.sort((a,b) => (
+                (new Date(a.start_time)).getTime() - (new Date(b.start_time)).getTime()
+            ))
+            activeEvents.push(...pastEvents)
+            this.setState({events: activeEvents})
+        })
     }
 
     componentDidMount(){
@@ -44,7 +82,8 @@ export default class Dashboard extends React.Component {
             <div className="container">
                 <h1>Dashboard</h1>
                 <div className="form">
-                    <form onSubmit={this.getEvents}>
+                    <form onSubmit={this.handleSubmit}>
+                        {/* <ValidateGroup> */}
                         <label htmlFor="after">
                         Starting Time:
                         </label><br/>
@@ -53,7 +92,8 @@ export default class Dashboard extends React.Component {
                         Ending Time:
                         </label><br/>
                         <DateTime id="before" inputProps={{placeholder: (new Date()).toLocaleString()}}/>
-                        <button>Submit</button>
+                        <button type="submit">Submit</button>
+                        {/* </ValidateGroup> */}
                     </form>
                 </div>
                 <div className="event-display">
